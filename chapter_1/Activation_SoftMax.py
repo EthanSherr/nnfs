fixing activation softmax to not explode!

chapter_1/Activation_SoftMax.py
@@ -25,6 +25,8 @@ class Activation_SoftMax_One:
 
 class Activation_SoftMax:
     def forward(self, inputs):
-        exp_values = np.exp(inputs)
+        # e^(inputs ^ row max(inputs)) for each input, keeping dims! (subtracting keeps output in interval [0, 1], otherwise e^1000 is too big...etc)
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        # normalize
         exp_sum = exp_values / np.sum(exp_values, axis=1, keepdims=True)
         self.output = exp_sum